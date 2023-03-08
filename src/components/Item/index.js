e88499ea694ce5c91d81b7cc87ed02e382a8ca17
Frank Quintana <ferickquintana@gmail.com>
2023-03-08 06:29:53 -0300

    adicion item, itemlist

---
+++ src/components/Item/index.js
@@ -0,0 +1,13 @@
+import "./item.css";
+import React from "react";
+
+const Item = () => {
+    return (
+        <a href="" className="case">
+            <img src="../assets/iph-11.jpg" alt="" />
+            <p>Fundas iPhone 11</p>
+        </a>
+    )
+}
+
+export default Item;
--- src/components/ItemListContainer/index.js
+++ src/components/ItemListContainer/index.js
@@ -1,13 +1,46 @@
-import "./itemList.css";
-import React from "react";
+import "./itemListContainer.css";
+import ItemList from "../ItemList";
+import React, {useState, useEffect} from "react";
+
+const cover = [
+    {
+        id: 1,
+        image: "../assets/iph-11.jpg",
+        title: "Funda iPhone 11"
+    },
+    {
+        id: 2,
+        image: "../assets/iph-13.jpg",
+        title: "Funda iPhone 11"
+    },
+    {
+        id: 3,
+        image: "../assets/iph-14.jpg",
+        title: "Funda iPhone 11"
+    }
+
+];
 
 export const ItemListContainer = ({greeting}) => {
 
+    const [data, setData] =useState([]);
+
+    useEffect(() => {
+        const getData = new Promise(resolve => {
+            setTimeout(() => {
+                resolve(cover);
+            }, 3000);
+        });
+        getData.then(res => console.log(res))
+    }, []);
 
     return (
-        <div className="item__container">
-            <h1>{greeting}</h1>
-        </div>
+        <>
+            <div className="item__container">
+                <h1>{greeting}</h1>
+            </div>
+            <ItemList data={data} />
+        </>
     );
 }
 
