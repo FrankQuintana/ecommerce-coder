ae2849bdf478b06a426c77d65bdf72c8c88af992
Frank Quintana <ferickquintana@gmail.com>
2023-04-25 21:34:55 -0300

    NavBar y estilos

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import './navBar.css'
 
 export const NavBar = () => {
   return (
-    <h1>NavBar</h1>
+    <div className='container'>
+      <nav className='nav'>
+        <div className='brand'>
+          <a className='nav__link' href='#'>CeluMusic</a>
+        </div>
+        <ul className='nav__list'>
+          <li>
+            <a className='nav__link' href="#">Fundas</a>
+          </li>
+          <li>
+            <a className='nav__link' href="#">Cargadores</a>
+          </li>
+          <li>
+            <a className='nav__link' href="#">Carro</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
-export default NavBar
+export default NavBar;
